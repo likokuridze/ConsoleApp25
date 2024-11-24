47c92cc9d4e4788116444d1697faac4bd652d340
LikoKuridze <lkuridze@credo.ge>
2024-11-24 23:05:30 +0400

    Add project files.

---
+++ ConsoleApp25/Counter.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp25
+{
+    internal class Counter
+    {
+        static int Count = 0;
+
+        public Counter()
+        {
+            Count++;
+        }
+
+        public static int GetCount()
+        {
+            return Count;
+        }
+
+        public static void ResetCount()
+        {
+            Count = 0;
+        }
+    }
+}
+
---
+++ ConsoleApp25/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace ConsoleApp25;
+
+  class Program
+{
+    static void Main(string[] args)
+    {
+        Counter c1 = new Counter();
+        int count1 = Counter.GetCount();
+        Console.WriteLine($"პირველი ობიექტის შემდეგ: {count1}");
+
+        Counter c2 = new Counter();
+        int count2 = Counter.GetCount();
+        Console.WriteLine($"მეორე ობიექტის შემდეგ: {count2}");
+
+        Counter.ResetCount();
+        int resetCount = Counter.GetCount();
+        Console.WriteLine($"განულების შემდეგ: {resetCount}");
+    }
+}
